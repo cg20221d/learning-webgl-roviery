ccfdd837c6ea597fde8bb1254c5aad98b6e33297
Nathanael Roviery <roviery@gmail.com>
2022-09-14 15:15:07 +0700

    add drawArrays

--- main.js
+++ main.js
@@ -2,17 +2,38 @@ function main() {
     var kanvas = document.getElementById("kanvas");
     var gl = kanvas.getContext("webgl");
 
+    var vertices = [
+        0.5, 0.5,   // A: kanan atas
+        0.0, 0.0,   // B: bawah tengah
+        -0.5, 0.5   // C: kiri atas
+    ];
+
+    var buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
     // Vertex shader
-    var vertexShaderCode = 
-    "void main() {" + 
-    "}";
+    var vertexShaderCode =  `
+    attribute vec2 aPosition;
+    void main() {
+        float x = aPosition.x;
+        float y = aPosition.y;
+        gl_PointSize = 10.0;
+        gl_Position = vec4(x, y, 0.0, 1.0);
+    }
+    `;
     var vertexShaderObject = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShaderObject, vertexShaderCode);
     gl.compileShader(vertexShaderObject);   // sampai sini sudah jadi .o
 
     // Fragment shader
     var fragmentShaderCode = `
+    precision mediump float;
     void main() {
+        float r = 0.0;
+        float g = 0.0;
+        float b = 1.0;
+        gl_FragColor = vec4(r, g, b, 1.0);
     }
     `;
     var fragmentShaderObject = gl.createShader(gl.FRAGMENT_SHADER);
@@ -25,7 +46,20 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
-    gl.clearColor(1.0,      0.65,    0.0,    1.0);
+    // Kita mengajari GPU bagaimana caranya mengoleksi
+    //  nilai posisi dari ARRAY_BUFFER
+    //  untuk setiap verteks yang sedang diproses
+    var aPosition = gl.getAttribLocation(shaderProgram, "aPosition");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
+    gl.clearColor(1.0,      0.65,    0.0,    1.0);  // Oranye
     //            Merah     Hijau   Biru    Transparansi
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 3);
+
+    gl.drawArrays(gl.LINE_LOOP, 0, 3);
+
+    gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
